ab35af1a776b47215939a70605a8eba75cd13c1f
Christopher R. Palmer <crpalmer@gmail.com>
2023-02-17 08:41:49 -0500

    PotionOfHealing

---
+++ Potions/PotionOfHealing.gd
@@ -0,0 +1,8 @@
+extends InventoryThing
+
+export var healing_dice = { "n": 2, "d": 4, "plus": 4 }
+
+func used_by(who:Actor):
+	if who.has_method("give_hit_points") and may_use():
+		.used_by(who)
+		who.give_hit_points(GameEngine.roll(healing_dice))
