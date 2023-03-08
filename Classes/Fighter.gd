f63ee395ccf08bf69adf9124e57e31e4169c67e8
Christopher R. Palmer <crpalmer@gmail.com>
2023-03-08 06:10:41 -0500

    fighter: fix use of match in level attainments

--- Classes/Fighter.gd
+++ Classes/Fighter.gd
@@ -7,20 +7,19 @@ func initial_hit_points(): return 10
 
 func ability_improvement_at_level(level):
 	match(level):
-		1-3: return 1
-		4-5: return 2
-		6-7: return 3
-		8-11: return 4
-		12-15: return 5
-		16-18: return 6
-		20-9999: return 7
+		0,1,2,3: return 0
+		4,5: return 1
+		6,7: return 2
+		8,9,10,11: return 3
+		12,13,14,15: return 4
+		16, 17, 18: return 5
+	return 6
 		
 func number_of_attacks(level):
-	match(level):
-		1-4: return 1
-		5-10: return 2
-		11-19:  return 3
-		20-99999: return 4
+	if level <= 4: return 1
+	if level <= 10: return 2
+	if level <= 19: return 3
+	return 4
 
 func strength_modifier(strength, level):
 	return super.strength_modifier(strength, level) + profiency_modifier(level)
